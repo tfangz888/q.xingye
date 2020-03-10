52f4597c97e3fb1e1f37a7c6ec91de3e556fda6d
Tfangz <tfangz@ymail.com>
2020-03-10 22:46:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ cp feedhandler/xingye_feed.q ~/q/feedhandler/
 
 sudo cp xingye_feed.service /etc/systemd/system/
 sudo cp xingye_feed.timer /etc/systemd/system/
+sudo cp xingye_tickerplant.service /etc/systemd/system/
+sudo cp xingye_tickerplant.timer /etc/systemd/system/
