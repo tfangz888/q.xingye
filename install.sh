5892561367ac264b3fcad70e58503eea3f768941
Tfangz <tfangz@ymail.com>
2020-03-10 23:18:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 cp tick/symxingye.q ~/q/tick/
 cp feedhandler/xingye_feed.q ~/q/feedhandler/
 
+cp xingye_tickerplant.sh ~/q/
+cp xingye_rdb.sh ~/q/
+
 sudo cp xingye_feed.service /etc/systemd/system/
 sudo cp xingye_feed.timer /etc/systemd/system/
 sudo cp xingye_tickerplant.service /etc/systemd/system/
 sudo cp xingye_tickerplant.timer /etc/systemd/system/
+sudo cp xingye_rdb.service /etc/systemd/system/
+sudo cp xingye_rdb.timer /etc/systemd/system/
