dfabb76d554e04242c119c1f65396ecd90a1dd14
Tfangz <tfangz@ymail.com>
2020-04-21 23:19:35 +0800

    Update xingye_tickerplant.sh

--- xingye_tickerplant.sh
+++ xingye_tickerplant.sh
@@ -3,6 +3,6 @@
 cd /home/toby/q
 
 # tickerplant
-# history log in /media/sf_e/data/datasource
+# history log in /home/toby/data/datasource/xingye
 # 注意不要带斜杠/，否则运行RDB时会出错
 /home/toby/q/l32/q tick.q symxingye /home/toby/data/datasource/xingye  -p 5010 </dev/null >/dev/null 2>&1 
