9d2a3d9fa1962288b0860815346bc497d0673122
Tfangz <tfangz@ymail.com>
2020-10-12 16:18:21 +0800

    Update xingye_rdb.sh

--- xingye_rdb.sh
+++ xingye_rdb.sh
@@ -2,4 +2,5 @@
 
 cd /home/toby/q
 # rdb
-/home/toby/q/l32/q tick/r.q :5010 -p 5011 </dev/null >/dev/null 2>&1
+# /home/toby/q/l32/q tick/r.q :5010 -p 5011 </dev/null >/dev/null 2>&1
+q32 tick/r.q :5010 -p 5011 </dev/null >/dev/null 2>&1
